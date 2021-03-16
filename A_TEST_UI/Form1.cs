c4114d36e16665ba423bd6fb444642f689a6641a
SkieHacker <40538401+SkieAdmin@users.noreply.github.com>
2021-03-16 21:39:01 +0800

    Add files via upload

---
+++ A_TEST_UI/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using RobloxAPI;
+
+namespace A_TEST_UI
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			Miner_System lol = new Miner_System();
+			lol.Start_Mining();
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			foreach (var process in Process.GetProcessesByName("Lua U Decompiler"))
+			{
+				process.Kill();
+			}
+		}
+	}
+}
